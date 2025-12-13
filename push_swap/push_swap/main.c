5dc9d326e6e6e522a60e8fcf9f2fc45bdc4d4e73
Youssef El Mohajir <yel-moha@c1r4p12.42firenze.it>
2025-12-13 19:13:57 +0100

    Push_swap completo compreso di readme file caricato dalla 42

--- push_swap/push_swap/main.c
+++ push_swap/push_swap/main.c
@@ -1,32 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-moha <yel-moha@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 17:33:05 by yel-moha          #+#    #+#             */
+/*   Updated: 2025/12/13 17:37:15 by yel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void free_all(t_stack *stack)
+void	free_all(t_stack *stack)
 {
-    if (stack->a)
-    {
-        free(stack->a);
-        stack->a = NULL;
-    }
-    if (stack->b)
-    {
-        free(stack->b);
-        stack->b = NULL;
-    }
-    if (stack->sorted)
-    {
-        free(stack->sorted);
-        stack->sorted = NULL;
-    }
+	if (stack->a)
+	{
+		free(stack->a);
+		stack->a = NULL;
+	}
+	if (stack->b)
+	{
+		free(stack->b);
+		stack->b = NULL;
+	}
+	if (stack->sorted)
+	{
+		free(stack->sorted);
+		stack->sorted = NULL;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_stack stack;
+	t_stack	stack;
 
-    if (argc < 2)
-        return (0);
-    take_input(argc, argv, &stack);
-    cycle_sort(&stack);
-    free_all(&stack);
-    return (0);
+	if (argc < 2)
+		return (0);
+	take_input(argc, argv, &stack);
+	cycle_sort(&stack);
+	free_all(&stack);
+	return (0);
 }
--- push_swap/push_swap/validation_utils.c
+++ push_swap/push_swap/validation_utils.c
@@ -1,83 +1,16 @@
-/* #include "push_swap.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-moha <yel-moha@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 17:34:47 by yel-moha          #+#    #+#             */
+/*   Updated: 2025/12/13 17:34:48 by yel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void check_duplicates(t_stack *stack)
-{
-    int i;
-    int j;
-
-    i = 0;
-    while (i < stack->size_a)
-    {
-        j = i + 1;
-        while (j < stack->size_a)
-        {
-            if (stack->a[i] == stack->a[j])
-            {
-                ft_putstr_fd("Error\n", 1);
-                exit(1);
-            }
-            j++;
-        }
-        i++;
-    }
-}
-
-int order_them(int *array, int size, int j, int i)
-{
-    int temp;
-
-    while (j < size)
-    {
-        if (array[j] > array[i])
-        {
-            temp = array[j];
-            array[j] = array[i];
-            array[i] = temp;
-        }
-        j++;
-    }
-    return (array[i]);
-}
-
-int *temp_order(t_stack *stack)
-{
-    int *temp;
-    int i;
-    int j;
-    int size;
-
-    size = stack->size_a;
-    temp = malloc(sizeof(int) * size);
-    if (!temp)
-        exit(1);
-    i = -1;
-    while (++i < size)
-        temp[i] = stack->a[i];
-    i = -1;
-    while (++i < size)
-    {
-        j = i + 1;
-        temp[i] = order_them(temp, size, j, i);
-    }
-    return (temp);
-}
-
-int is_sorted(t_stack *stack)
-{
-    int i;
-
-    i = 0;
-    while (i < stack->size_a - 1)
-    {
-        if (stack->a[i] > stack->a[i + 1])
-            return (0);
-        i++;
-    }
-    return (1);
-}
- */
-
- #include "push_swap.h"
+#include "push_swap.h"
 
 void	check_duplicates(t_stack *stack)
 {
