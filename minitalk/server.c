ccfe88208b092f8e1feba9efbabb8672cd9cd45a
Youssef El Mohajir <yel-moha@c1r4p1.42firenze.it>
2025-12-20 13:15:07 +0100

    fix: convert minitalk from nested repo to normal directory

---
+++ minitalk/server.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-moha <yel-moha@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 13:01:39 by yel-moha          #+#    #+#             */
+/*   Updated: 2025/12/15 13:01:46 by yel-moha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+volatile sig_atomic_t	g_state[2] = {0, 0};
+
+static void	write_pid(void)
+{
+	char	buf[32];
+	pid_t	pid;
+	int		i;
+
+	pid = getpid();
+	i = 0;
+	if (pid == 0)
+		buf[i++] = '0';
+	while (pid > 0)
+	{
+		buf[i++] = (pid % 10) + '0';
+		pid /= 10;
+	}
+	while (i-- > 0)
+		write(1, &buf[i], 1);
+	write(1, "\n", 1);
+}
+
+static void	signal_handler(int signo)
+{
+	char	c;
+
+	g_state[0] = (g_state[0] << 1) | (signo == SIGUSR2);
+	g_state[1]++;
+	if (g_state[1] == 8)
+	{
+		c = (char)g_state[0];
+		if (c == '\0')
+			write(1, "\n", 1);
+		else
+			write(1, &c, 1);
+		g_state[0] = 0;
+		g_state[1] = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	write_pid();
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
